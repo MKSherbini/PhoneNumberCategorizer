048a5c6079d1a3de16764f1674a2d09a9048486d
MKSherbini <mksherbini@gmail.com>
2022-02-16 11:10:06 +0200

    Added ExceptionAdvice to handle exceptions

---
+++ Backend/src/main/java/com/mksherbini/backend/controllers/ExceptionAdvice.java
@@ -0,0 +1,32 @@
+package com.mksherbini.backend.controllers;
+
+import com.mksherbini.backend.models.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex, HttpServletRequest req) {
+        String name = ex.getName();
+        String type = "unknown";
+        if (ex.getRequiredType() != null)
+            type = ex.getRequiredType().getSimpleName();
+        Object value = ex.getValue();
+        String message = String.format("'%s' should be a valid '%s' and '%s' isn't",
+                name, type, value);
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(message);
+        errorResponse.setStatus(HttpStatus.valueOf(400));
+        errorResponse.setCode(400);
+        errorResponse.setLocation(req.getRequestURL().toString());
+        return new ResponseEntity<>(errorResponse,
+                errorResponse.getStatus());
+    }
+}
